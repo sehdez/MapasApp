834a66487f3c994bad9f8379515de22bbc1b0f5b
sehdez <sergio.hdez.cuevas@gmail.com>
2022-03-05 01:12:02 -0600

    Fin del proyecto

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styles: [
-  ]
+  styles: [`
+      div{
+        width:100%;
+        height: 100px;
+        margin: 0;
+      }
+  `]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
 
-  constructor() { }
+  @Input() lngLat: [number,number] = [0,0];
+  @ViewChild('mapa') divMapa! : ElementRef;
 
-  ngOnInit(): void {
+
+
+
+  ngAfterViewInit(): void {
+
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive:false
+    });
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(mapa);
+      
   }
 
+  constructor() { }
 }
--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+interface Propiedad {
+  titulo: string;
+  descripcion: string;
+  lngLat: [number, number];
+}
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-propiedades',
@@ -6,11 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PropiedadesComponent implements OnInit {
+export class PropiedadesComponent {
 
-  constructor() { }
+  propiedades: Propiedad[] = [
+    {
+      titulo: 'Domicilio del desarrollador, México',
+      descripcion: 'Un excelente lugar para desarrollar proyectos',
+      lngLat: [  -103.2670126610955, 20.551366902623]
+    },
+    {
+      titulo: 'Apartamento, Argentina',
+      descripcion: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
+      lngLat: [ -58.430166677283445, -34.57150108832866 ]
+    },
+    {
+      titulo: 'Casa de playa, México',
+      descripcion: 'Hermosa casa de playa en Acapulco, México',
+      lngLat: [ -99.91287720907991, 16.828940930185748]
+    },
+    {
+      titulo: 'Local comercial, España',
+      descripcion: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
+      lngLat: [ -3.7112735618380177, 40.42567285425766 ]
+    },
+  ]
 
-  ngOnInit(): void {
-  }
 
 }
